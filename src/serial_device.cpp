e551700b69c17a426c371bb2d72bb01a4ab0ec29
dor666 <dorian.apanel@gmail.com>
2014-05-29 20:37:40 +0200

    Serial port tests passes

--- src/serial_device.cpp
+++ src/serial_device.cpp
@@ -1,5 +1,6 @@
 #include "serial_device.h"
 #include "ui_serial_device.h"
+#include <QSerialPort>
 
 namespace dplot
 {
@@ -13,9 +14,28 @@ SerialDevice::SerialDevice(SerialPortFactoryInterface* _portFactory, QWidget *pa
 
     RefreshPortList();
 
+    ui->dataBitsComboBox->addItem("5", QSerialPort::Data5);
+    ui->dataBitsComboBox->addItem("6", QSerialPort::Data6);
+    ui->dataBitsComboBox->addItem("7", QSerialPort::Data7);
+    ui->dataBitsComboBox->addItem("8", QSerialPort::Data8);
+    ui->dataBitsComboBox->setCurrentIndex(3);
 
-    //ui->dataBitsComboBox->ins
+    ui->parityComboBox->addItem(tr("None"), QSerialPort::NoParity);
+    ui->parityComboBox->addItem(tr("Even"), QSerialPort::EvenParity);
+    ui->parityComboBox->addItem(tr("Odd"), QSerialPort::OddParity);
+    ui->parityComboBox->addItem(tr("Space"), QSerialPort::SpaceParity);
+    ui->parityComboBox->addItem(tr("Mark"), QSerialPort::MarkParity);
+    ui->parityComboBox->setCurrentIndex(0);
 
+    ui->stopBitsComboBox->addItem(tr("1"), QSerialPort::OneStop);
+    ui->stopBitsComboBox->addItem(tr("1.5"), QSerialPort::OneAndHalfStop);
+    ui->stopBitsComboBox->addItem(tr("2"), QSerialPort::TwoStop);
+    ui->stopBitsComboBox->setCurrentIndex(0);
+
+    ui->flowControlComboBox->addItem(tr("None"), QSerialPort::NoFlowControl);
+    ui->flowControlComboBox->addItem(tr("Hardware"), QSerialPort::HardwareControl);
+    ui->flowControlComboBox->addItem(tr("Software"), QSerialPort::SoftwareControl);
+    ui->flowControlComboBox->setCurrentIndex(0);
 }
 
 void SerialDevice::RefreshPortList()
@@ -49,3 +69,5 @@ QStringList SerialPortFactory::EnumeratePorts()
 }
 
 } //namespace dplot
+
+
